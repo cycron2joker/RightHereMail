814267917429a4efd9e52791689764f0e5ea68db
merowlink <merowlink@gmail.com>
2013-12-08 22:35:10 +0900

    Initial Commit

---
+++ RightHereMail/GkGkAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GkGkAppDelegate.h
+//  RightHereMail
+//
+//  Created by pies on 2013/12/08.
+//  Copyright (c) 2013年 pies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GkGkAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RightHereMail/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RightHereMail
+//
+//  Created by pies on 2013/12/08.
+//  Copyright (c) 2013年 pies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GkGkAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GkGkAppDelegate class]));
+    }
+}
